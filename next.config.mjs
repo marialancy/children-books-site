adbaf5e54a47044ced80f99371c83212755fc9a0
lancy2118 <lancy2118@gmail.com>
2024-10-20 14:59:16 -0500

    update github pages build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/children-books-site', // Adjust for GitHub Pages
+    images: {
+      unoptimized: true, // Disable image optimization for GitHub Pages
+    },
+};
 
 export default nextConfig;
