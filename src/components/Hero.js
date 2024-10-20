8a0fc3e0927796fe3848e5f10f0d0de28a350340
lancy2118 <lancy2118@gmail.com>
2024-10-20 14:53:49 -0500

    replace quotes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,9 @@ export default function Hero() {
       <div className="bg-white py-16">
         <div className="container mx-auto md:flex items-center">
           <div className="md:w-1/2 mb-8 md:mb-0">
-            <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">Top Children's Books</h1>
+            <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">Top Children&apos;s Books</h1>
             <p className="text-xl text-gray-600 mb-8">
-            Explore a curated selection of the best kids' books—perfect for every child's interests and reading level.
+            Explore a curated selection of the best kids&apos; books—perfect for every child&apos;s interests and reading level.
             </p>
             <button className="bg-blue-500 text-white py-3 px-8 rounded-full hover:bg-blue-600 transition duration-300">
               Explore
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -3,9 +3,9 @@ export default function Newsletter() {
       <section className="py-16 bg-blue-50">
         <div className="container mx-auto md:flex items-center">
           <div className="md:w-1/2 mb-8 px-6 md:mb-0">
-            <h2 className="text-3xl font-bold text-gray-800 mb-4">Support Your Child's Reading Journey at Home</h2>
+            <h2 className="text-3xl font-bold text-gray-800 mb-4">Support Your Child&apos;s Reading Journey at Home</h2>
             <p className="text-xl text-gray-600 mb-4">
-            With KidsReadWorld's newsletter, you'll receive free, research-backed reading resources, activities, and more, all designed to help your child thrive. Sign up today and start exploring!
+            With KidsReadWorld&apos;s newsletter, you&apos;ll receive free, research-backed reading resources, activities, and more, all designed to help your child thrive. Sign up today and start exploring!
             </p>
             <div className="flex">
               <input type="email" placeholder="Enter your email" className="flex-grow p-3 rounded-l-lg" />
